1b4cfafb14863431735c8d619398ff5935d60628
RogerPierre <rogerpierrereis@gmail.com>
2026-04-16 13:49:07 +0000

    backup of progress

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,7 @@
 
 const typeAREA: HTMLInputElement= document.getElementById('typeAREA') as HTMLInputElement
 const MoneyConteiner: HTMLDivElement= document.getElementById('GRID-container-money') as HTMLDivElement;
-MoneyConteiner.innerHTML=`
-            <div class="moneyOutput" id="Output-dollar">
-                <h2 class='money-name' >DOLLAR</h2>
-                <p class="value-output">
-                    U$20.00
-                </p>
-            </div>
-            <div class="moneyOutput" id="Output-eur">
-                <h2 class='money-name'>EURO</h2>
-                <p class="value-output">
-                    EU$20.00
-                </p>
-            </div>
-            <div class="moneyOutput" id="Output-BTC">
-                <h2 class='money-name'>BITCOIN</h2>
-                <p class="value-output">
-                    BTC$20.00
-                </p>
-            </div>
-`
+
 interface RESmoedas{
     "USDBRL": resAPi,
     "EURBRL": resAPi,
@@ -51,24 +32,4 @@ async function requestAPIEconomic():Promise<RESmoedas>{
 typeAREA?.addEventListener('input',(ev:InputEvent)=>{
     let valueInput= Number(typeAREA.value);
     let res: RESmoedas= requestAPIEconomic() as unknown as RESmoedas;
-    MoneyConteiner.innerHTML=`
-            <div class="moneyOutput" id="Output-dollar">
-                <h2 class='money-name' >DOLLAR</h2>
-                <p class="value-output">
-                    U$${valueInput*Number(res.USDBRL.high)}
-                </p>
-            </div>
-            <div class="moneyOutput" id="Output-eur">
-                <h2 class='money-name'>EURO</h2>
-                <p class="value-output">
-                    EU$${valueInput*Number(res.EURBRL.high)}
-                </p>
-            </div>
-            <div class="moneyOutput" id="Output-BTC">
-                <h2 class='money-name'>BITCOIN</h2>
-                <p class="value-output">
-                    BTC$${valueInput*Number(res.BTCBRL.high)}
-                </p>
-            </div>
-`
-})
+});
