6ffff0354cf0dd5f4716730b9fd951ccd9b784de
RogerPierre <rogerpierrereis@gmail.com>
2026-04-17 17:37:44 +0000

    fiX others monetary VALUES.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { APIreq } from "./Protocol";
 const typeAREA: HTMLInputElement= document.getElementById('typeAREA') as HTMLInputElement
 const [BitcoinDiv,DOLLARdiv,EUROdiv]=[
     document.getElementById('Output-BTC') as HTMLDivElement,
-    document.getElementById('Output-dollar') as HTMLDivElement,
-    document.getElementById('Output-eur') as HTMLDivElement
+    document.getElementById('Output-USD') as HTMLDivElement,
+    document.getElementById('Output-EUR') as HTMLDivElement
 ]
 
    
@@ -16,23 +16,23 @@ typeAREA?.addEventListener('input',(ev:InputEvent)=>{
         BitcoinDiv.innerHTML= `
             <h2 class='money-name' >${data[2]!.code}</h2>
                 <p class="value-output">
-                    ${data[2]!.codein+((data[2]!.high)*value).toFixed(2)}
+                    ${data[2]!.codein+(1/(data[2]!.high)*value).toFixed(2)}
                 </p>
         `
         EUROdiv.innerHTML= `
             <h2 class='money-name' >${data[1]!.code}</h2>
                 <p class="value-output">
-                    ${data[1]!.codein+((data[2]!.high)*value)}
+                    ${data[1]!.codein+(1/(data[1]!.high)*value).toFixed(2)}
                 </p>
         `
         DOLLARdiv.innerHTML= `
             <h2 class='money-name' >${data[0]!.code}</h2>
                 <p class="value-output">
-                    ${data[0]!.codein+((data[0]!.high)*value)}
+                    ${data[0]!.codein+(1/(data[0]!.high)*value).toFixed(2)}
                 </p>
         `
     }
     ).catch((err)=>{
         console.error(err);
-    })})
+})})
     
